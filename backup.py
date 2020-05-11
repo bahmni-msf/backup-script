080096dfd7c6843ed4f01e1c3ea37e0afa07cbe3
surajkumardas <surajkumar.das@thoughtworks.com>
2020-05-11 17:28:50 +0530

    Added Incrimental dir for nagios and bahmni-mart

--- backup.py
+++ backup.py
@@ -1,14 +1,16 @@
 import os
 
 release_version = 'rel_1.2'
+pgsql_version = '0.92'
 
 
-def backup(version):
+def backup(version, pgsql_ver):
     # Update the release version to current one.
     with open("backup_cmd", 'r') as o, open('temp_backup_file', 'w') as p:
         content = o.read()
         o.seek(0)
         p.write(content.replace("release_version", str(version)))
+        p.write(content.replace("pgsql_version", str(pgsql_ver)))
 
     # Execute unix commands for backup for the current release
     with open("temp_backup_file", 'r') as p:
@@ -23,6 +25,6 @@ def backup(version):
     os.remove('temp_backup_file')
 
 
-backup(release_version)
+backup(release_version, pgsql_version)
 
 
