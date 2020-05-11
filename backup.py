9c59b57fd3d5ab7fb1d8cfce0c88721bebe215f6
surajkumardas <surajkumar.das@thoughtworks.com>
2020-05-11 16:08:57 +0530

    User input for the version name/id

--- backup.py
+++ backup.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess
 
 release_version = input('Enter Current release Version : ')
 
@@ -7,7 +8,7 @@ release_version = input('Enter Current release Version : ')
 with open("backup_cmd", 'r') as o, open('temp_backup_file', 'w') as p:
     content = o.read()
     o.seek(0)
-    p.write(content.replace("release_version", release_version))
+    p.write(content.replace("release_version", str(release_version)))
 
 # Execute unix commands for backup for the current release
 with open("temp_backup_file", 'r') as p:
@@ -16,6 +17,7 @@ with open("temp_backup_file", 'r') as p:
             if line.startswith("#"):
                 print("'\033[1;33m {}\033[0m'".format(line))
             else:
+                # subprocess.check_output(line, shell=False)
                 os.system('{}'.format(line))
 
 os.remove('temp_backup_file')
