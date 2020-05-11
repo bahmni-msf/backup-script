66550e4e56aba1a95cd865b34d0477fc143c9418
surajkumardas <surajkumar.das@thoughtworks.com>
2020-05-11 16:21:56 +0530

    User input removed

--- backup.py
+++ backup.py
@@ -1,23 +1,28 @@
-import sys
 import os
-import subprocess
 
-release_version = input('Enter Current release Version : ')
+release_version = 'rel_1.2'
 
-# Update the release version to current one.
-with open("backup_cmd", 'r') as o, open('temp_backup_file', 'w') as p:
-    content = o.read()
-    o.seek(0)
-    p.write(content.replace("release_version", str(release_version)))
 
-# Execute unix commands for backup for the current release
-with open("temp_backup_file", 'r') as p:
-    for line in p:
-        if line:
-            if line.startswith("#"):
-                print("'\033[1;33m {}\033[0m'".format(line))
-            else:
-                # subprocess.check_output(line, shell=False)
-                os.system('{}'.format(line))
+def backup(version):
+    # Update the release version to current one.
+    with open("backup_cmd", 'r') as o, open('temp_backup_file', 'w') as p:
+        content = o.read()
+        o.seek(0)
+        p.write(content.replace("release_version", str(version)))
 
-os.remove('temp_backup_file')
+    # Execute unix commands for backup for the current release
+    with open("temp_backup_file", 'r') as p:
+        for line in p:
+            if line:
+                if line.startswith("#"):
+                    print("'\033[1;33m {}\033[0m'".format(line))
+                else:
+                    # subprocess.check_output(line, shell=False)
+                    os.system('{}'.format(line))
+
+    os.remove('temp_backup_file')
+
+
+backup(release_version)
+
+
