4c90c4869cf64c9c3825176729a5ad66e78373b0
surajkumardas <surajkumar.das@thoughtworks.com>
2020-05-11 10:59:37 +0530

    User input for the release version

--- backup.py
+++ backup.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-release_version = "1.0"
+release_version = input('Enter Current release Version : ')
 
 # Update the release version to current one.
 with open("backup_cmd", 'r') as o, open('temp_backup_file', 'w') as p:
