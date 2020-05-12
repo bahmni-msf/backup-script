af9f47889b46416771d27ac3ff28b844720cf60c
surajkumardas <surajkumar.das@thoughtworks.com>
2020-05-12 15:39:11 +0530

    Changed the method to read file

--- backup.py
+++ backup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 release_version = 'rel_1.2'
 pgsql_version = '9.6'
@@ -11,13 +12,21 @@ def backup(version, pgsql_ver):
             content = o.read()
             p.write(content.replace("release_version", str(version)))
 
-    with open('temp_backup_file', 'r+') as p:
-        content = p.read()
-        p.seek(0)
-        p.write(content.replace("pgsql_version", str(pgsql_ver)))
+    # Not using with as there is some compatibility issue with 2.6 to replace the pgsql version
+    read_file = open("temp_backup_file", "r")
+    new_file_content = ""
+    for line in read_file:
+        stripped_line = line.strip()
+        new_line = stripped_line.replace("pgsql_version", pgsql_ver)
+        new_file_content += new_line + "\n"
+    read_file.close()
 
+    writing_file = open("temp_backup_file", "w")
+    writing_file.seek(0)
+    writing_file.write(new_file_content)
+    writing_file.close()
 
-    #Execute unix commands for backup for the current release
+    # Execute unix commands for backup for the current release
     with open("temp_backup_file", 'r') as p:
         for line in p:
             if line:
@@ -28,6 +37,7 @@ def backup(version, pgsql_ver):
                     os.system('{0}'.format(line))
 
     os.remove('temp_backup_file')
+    exit('Backup phase Completed')
 
 
 backup(release_version, pgsql_version)
